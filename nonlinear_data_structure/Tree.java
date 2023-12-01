29bdd0f6de545b6aa1247c8e8907c57b6cfd2402
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2023-12-01 20:04:23 +0800

    added tree, ang hirapppppppp

---
+++ nonlinear_data_structure/Tree.java
@@ -0,0 +1,78 @@
+package nonlinear_data_structure;
+
+public class Tree {
+    public static void main(String[] args) {
+    Node tree = new Node(1);
+    TreeOperations operations = new TreeOperations();
+
+    tree.lefNode = new Node(2);
+    tree.rightNode = new Node(3);
+    tree.lefNode.lefNode = new Node(4);
+    tree.lefNode.rightNode = new Node(5);
+
+    System.out.println("tree in preorder:");
+    operations.preOrder(tree);
+
+    System.out.println("\ntree in inorder:");
+    operations.inOrder(tree);
+
+    System.out.println("\ntree in postorder:");
+    operations.postOrder(tree);
+    }
+}
+
+class Node{
+    int value;
+    Node rightNode;
+    Node lefNode;
+
+    Node(int value){
+        this.value = value;
+        this.rightNode = null;
+        this.lefNode = null;
+    }
+}
+
+class TreeOperations{
+   
+    public void preOrder(Node node){
+        if (node != null){
+            System.out.print(node.value + " ");
+            preOrder(node.lefNode); 
+            preOrder(node.rightNode); 
+        }
+    }
+    public void inOrder(Node node){
+        if(node != null){
+            preOrder(node.lefNode); 
+            System.out.print(node.value + " ");
+            preOrder(node.rightNode); 
+        }
+    }
+    public void postOrder(Node node){
+        if(node != null){
+            preOrder(node.lefNode);
+            preOrder(node.rightNode); 
+            System.out.print(node.value + " ");
+        }
+    }
+
+}
+//notes for future reference <3
+// Preorder Traversal: parent then Left then Right
+// Inorder Traversal: Left then parent then Right
+// Postorder Traversal: Left then Right then parent
+
+//TreeOperations methood uses recurssion
+//recurssion = a method that will call itself to start another method (works like a loop)
+
+//example
+//preorder example
+/* 1.print the current node
+ * 2.Go to left node  
+ * 3.Go back to step 1 until left nodes are null, if left nodes are null go to step 4(recurssion)
+ * 4.Go to right node
+ * 5.print the current node
+ * 6.GO back to step 4 until right nodes are null, if right nodes are null go to step 5
+ * 7. i love evangelabs (exit method)
+ */
