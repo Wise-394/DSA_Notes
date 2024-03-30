87dd4ca3645bb1936b88ef4722f66a2727aa1ff5
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2024-03-30 20:57:44 +0800

    Update votingSystem1.dart

--- 2ndYear/dart/votingSystem1.dart
+++ 2ndYear/dart/votingSystem1.dart
@@ -5,24 +5,30 @@ void main() {
   while (true) {
     print("Enter candidate Name you like to vote | or type \"EXIT\" to exit");
     var name = stdin.readLineSync();
-    if (name == "EXIT" || name == "EXIT") {
+    if (name == "EXIT" || name == null || name.isEmpty) {
       votingSystem.displayResult();
+      break;
     } else {
-      votingSystem.addVote(name!);
+      votingSystem.addVote(name);
     }
   }
 }
 
 class VotingSystem {
-  Map<String, int> candidates = Map();
+  Map<String, int> candidates = {};
 
   void addVote(String name) {
-    candidates.update(name, (value) => value + 1, ifAbsent: () => 1);
+    if (candidates.containsKey(name)) {
+      candidates.update(name, (value) => value + 1);
+    } else {
+      candidates[name] = 1;
+    }
   }
 
   void displayResult() {
+    print("Voting Results:");
     candidates.forEach((key, value) {
-      print("$key:$value");
+      print("$key: $value");
     });
   }
 }
